b89ef3191cfa3eddf985017d807d62753284420d
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2021-02-06 23:37:55 +0900

    presenter structure

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,20 @@
-export default () => "Search";
+import PropTypes, { func } from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({ movieResults,
+    tvResults,
+    searchWord,
+    loading,
+    error,
+    handleSubmit }) => null;
+
+SearchPresenter.PropTypes = {
+    movieResults: PropTypes.array,
+    tvResults: PropTypes.array,
+    searchWord: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+    handleSubmit: PropTypes.func.isRequired
+}
+
+export default SearchPresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,16 @@
-export default () => "TV";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({ popular,
+    airingToday,
+    loading,
+    error }) => null;
+
+TVPresenter.PropTypes = {
+    popular: PropTypes.array,
+    airingToday: PropTypes.array,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string
+}
+
+export default TVPresenter;
